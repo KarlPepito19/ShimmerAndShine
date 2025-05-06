a3f25d789dfa9d8bda2a72645628f1404fa20d6c
Karl <karlvincentpepito31@gmail.com>
2025-05-06 12:42:28 +0800

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { FrontPageComponent } from './front-page/front-page.component';
 
-export const routes: Routes = [];
+export const appRoutes: Routes = [
+  { path: '', component: FrontPageComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent }
+];
+
